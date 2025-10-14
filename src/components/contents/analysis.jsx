cc564d33d1c0614d6c5653e1e0470fe947913a17
wally <waldifiaga8@gmail.com>
2025-10-14 12:44:54 +0200

    component analysis
    construction de la structure de analysis

---
+++ src/components/contents/analysis.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './analysis.scss';
+
+function Analysis() {
+    return (
+        <div className="analysis">
+            <div className="ana_choice">
+                <div className="ana_options">
+                    <div className="ana_option">Large Cranium</div>
+                    <div className="ana_option">Wide-angle vision</div>
+                    <div className="ana_option">Flaming heart</div>
+                    <div className="ana_option">Fairy fingers</div>
+                    <div className="ana_option">Solid Support</div>
+                </div>
+                <div className="ana_img"></div>
+            </div>
+            <div className="ana_answere">
+                <p>Impossible to know exactly what's hidden there, but many ideas are jostling around.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Analysis;
+
+
--- src/components/waldi.jsx
+++ src/components/waldi.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import Window from './window.jsx'
 import moi from '../assets/moi/WALDII.png';
 import './waldi.scss';
+import Analysis from './contents/analysis.jsx';
 
 
 function Waldi() {
   return (
     <div className="waldi lvl">
         <Window nom="recent_picture" id="waldi" contenu={<img src={moi} className="waldi__image" alt="Recent" />} />
-        <Window nom="link" id="link" />
-        <Window nom="softwares" id="softwares" />
+        <Window nom="link" id="link" /> {/* lien vers les reseaux et cv */}
+        <Window nom="softwares" id="softwares" /> {/* logiciels maitrisés */}
         <Window nom="waldi_ia" id="IA" />
         <Window nom="Time_travel" id="frise" />
-        <Window nom="Analysis" id="analysis" />
+        <Window nom="Analysis" id="analysis" contenu={<Analysis />} />
         <Window nom="auto_evaluation" id="skills" />
     </div>
   );
