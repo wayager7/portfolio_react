8f5fa30b7718caff5d21b4990a4d6cf2c53b8615
wally <120519860+wayager7@users.noreply.github.com>
2025-10-26 02:28:28 +0100

    logo de link
    solutions textuel temporaire pour l'explication de chaques liens

--- src/components/contents/wa_link.jsx
+++ src/components/contents/wa_link.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import './wa_link.scss';
 import cv from '../../assets/moi/cv2025_10_24.pdf';
+import ico_cv from '../../assets/moi/ico_cv.png';
+import ico_lkdn from '../../assets/moi/ico_lkdn.png';
+import ico_git from '../../assets/moi/ico_git.png';
+import ico_bento from '../../assets/moi/ico_bento.png';
 
 function Link() {
     return (
         <div className="link">
-            <a className="link_item" href={cv} target="_blank"></a>
-            <a className="link_item" href="https://www.linkedin.com/in/waldi-fiaga/" target="_blank"></a>
-            <a className="link_item" href="https://github.com/wayager7" target="_blank"></a>
-            <a className="link_item" href="https://bento.me/wayager" target="_blank"></a>
+            <a className="link_item" href={cv} target="_blank"> 
+                <img className="ico" src={ico_cv} alt="" />
+                <p>cv</p>
+            </a>
+            <a className="link_item" href="https://www.linkedin.com/in/waldi-fiaga/" target="_blank">
+                <img className="ico" src={ico_lkdn} alt="" />
+                <p>lkdn</p>
+            </a>
+            <a className="link_item" href="https://github.com/wayager7" target="_blank">
+                <img className="ico" src={ico_git} alt="" />
+                <p>git</p>
+            </a>
+            <a className="link_item" href="https://bento.me/wayager" target="_blank">
+                <img className="ico" src={ico_bento} alt="" />
+                <p>bento</p>
+            </a>
         </div>
     );
 }
