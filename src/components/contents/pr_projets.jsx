d8a04d7750582bf1f5ea37a36ee799bb3b2f39b1
wally <waldifiaga8@gmail.com>
2025-11-10 16:25:54 +0100

    starter de la zone projets
    premier json, image test, et placement au scss

---
+++ src/components/contents/pr_projets.jsx
@@ -0,0 +1,30 @@
+import react, { useState } from 'react';
+import projet from './data/pr_ol_projets.json';
+import './style/pr_projets.scss';
+
+function ProjetsList(){
+    const [hoveredId, setHoveredId] = useState(null);
+    return(
+
+        <div className='projets-list'>
+            {projet.map((p)=>(
+                <>
+                    <div key={p.id} className='projet'>
+                        <div className='imgdiv'>
+                            <img src={p.images} alt={p.name} />
+                        </div>
+                        <h3>{p.name}</h3>
+                        <p>{p.description}</p>
+                        <div className='tags'>
+                            {p.tags.map((tag) => (
+                                <span key={tag} className="tag">{tag}</span>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            ))}
+        </div>
+    )
+}
+
+export default ProjetsList;
--- src/components/projets.jsx
+++ src/components/projets.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Window from './window.jsx'
 import './style/projets.scss';
 import Travaux from "./contents/travaux.jsx";
+import ProjetsList from "./contents/pr_projets.jsx";
 
 
 
@@ -9,11 +10,11 @@ const Projets = () => {
     return (
         <>
         <div className="projets">
-            <Window nom="Projets" id="projets" contenu={<h2>Choose a project</h2>}/>
+            <Window nom="Projets" id="projets" contenu={<ProjetsList />}/>
             <Window nom="Projet_description" id="PD"/>
             <Window nom="Pictures" id="Pictures"/>
         </div>
-        <Travaux />
+        {/* <Travaux /> */}
         </>
     );
 };
