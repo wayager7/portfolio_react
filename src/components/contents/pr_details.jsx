633d2fa60791abd5d0830e71b810a79305604459
wally <120519860+wayager7@users.noreply.github.com>
2025-11-15 21:27:39 +0100

    maj projet affichages

--- src/components/contents/pr_details.jsx
+++ src/components/contents/pr_details.jsx
@@ -21,37 +21,57 @@ const Details = ({ prID }) => {
                         ))}
                     </div>
                     <p>{projetDetails.description}</p>
-                    <p>{projetDetails.date}</p>
                     {projetDetails.link && (
-                        <a href={projetDetails.link} target="_blank" rel="noopener noreferrer">Voir le projet</a>
+                        projetDetails.link.map((linkobj, index) =>(
+                            <>
+                                <a key={index} href={linkobj.url} target="_blank" rel="noopener noreferrer">{linkobj.type}</a> •
+                            </>
+                        ))
                     )}
+                    <p>{projetDetails.date}</p>
+
                 </div>
                 
             </div>
             <div className='bottom'>
-                {/* <h3>Informations about projet</h3> */}
-                {projetDetails.contexte && (
-                    <div className='section contexte'>
-                        <h4>contexte</h4>
-                        <p>{projetDetails.contexte}</p>
-                    </div>
-                )}
-                {projetDetails.objectif && (
-                    <div className='section objectif'>
-                        <h4>objectif</h4>
-                        <p>{projetDetails.objectif}</p>
-                    </div>
-                )}
-                {projetDetails.choix && (
-                    <div className='section choix'>
-                        <h4>choix</h4>
-                        <p>{projetDetails.choix}</p>
-                    </div>
-                )}
-                {projetDetails.bilan && (
-                    <div className='section bilan'>
-                        <h4>bilan</h4>
-                        <p>{projetDetails.bilan}</p>
+                <div className='left'>
+                    <h3>Informations about projet</h3>
+                    {projetDetails.contexte && (
+                        <div className='section contexte'>
+                            <h4>contexte</h4>
+                            <p>{projetDetails.contexte}</p>
+                        </div>
+                    )}
+                    {projetDetails.objectif && (
+                        <div className='section objectif'>
+                            <h4>objectif</h4>
+                            <p>{projetDetails.objectif}</p>
+                        </div>
+                    )}
+                    {projetDetails.choix && (
+                        <div className='section choix'>
+                            <h4>choix</h4>
+                            <p>{projetDetails.choix}</p>
+                        </div>
+                    )}
+                    {projetDetails.bilan && (
+                        <div className='section bilan'>
+                            <h4>bilan</h4>
+                            <p>{projetDetails.bilan}</p>
+                        </div>
+                    )}
+                </div>
+                
+                {projetDetails.team && (
+                    <div className='team'>
+                        <h4>team</h4>
+                        {projetDetails.team.map((member, index) => (
+                            <div key={index} className='member'>
+                                <a key={index} href={member.lkdn}>{member.nom}</a>
+                                <br />
+                                <span> - {member.role}</span>
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
