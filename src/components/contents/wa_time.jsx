6c123292bfb3e1f4121a376ced121ab596a3c61e
wally <120519860+wayager7@users.noreply.github.com>
2026-02-16 21:23:49 +0100

    mise a jour timetravel
    changement du logo sti2d, modification du style de la fleche pour plus de claretée

--- src/components/contents/wa_time.jsx
+++ src/components/contents/wa_time.jsx
@@ -5,7 +5,7 @@ function Time() {
     const [selectedOption, setSelectedOption] = useState(null);
     const [displayText, setDisplayText] = useState('');
     const infos = useMemo(() => ([
-        { id: "un", title: "BAC Science Technologie de l'Industrie et du Développement Durable", image: "/logoSTI.jpg", date: "2020 - 2022", description: "Filière technologique centrée sur l'innovation, la conception technique et l'ingénierie, tout en intégrant systématiquement les enjeux environnementaux et la transition énergétique."},
+        { id: "un", title: "BAC Science Technologie de l'Industrie et du Développement Durable", image: "/logoSTI2.jpg", date: "2020 - 2022", description: "Filière technologique centrée sur l'innovation, la conception technique et l'ingénierie, tout en intégrant systématiquement les enjeux environnementaux et la transition énergétique."},
         { id: "deux", title: "BUT Métiers du Multimédia et de l'Internet", image: "/logoMMI.png", date: "2022 - 2025", description: "Formation pluridisciplinaire en trois ans qui fusionne la création numérique, le développement web et la stratégie de communication."},
         { id: "trois", title: "MASTER Science Sociale Culture et Metiers du Web", image: "logoCMW.jpg", date: "2025 - 2027", description: "Formation des stratèges du numérique capables de piloter des projets complexes en mêlant design de communication, marketing digital et gestion technique de plateformes web."},
     ]), []);
